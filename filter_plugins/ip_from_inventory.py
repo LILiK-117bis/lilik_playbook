7eca6ef9509ae112a5384dfbf233ec9bd8713b9d
Edoardo Putti <edoardo.putti@gmail.com>
2017-08-27 16:57:03 +0200

    use check instead of try, except block in filter ip_from_address

--- filter_plugins/ip_from_inventory.py
+++ filter_plugins/ip_from_inventory.py
@@ -5,10 +5,10 @@ def ip_from_inventory(hostvars, hostname):
     replace this ``{{ hostvars[hostname]['ansible_host'] }}``
     with something nicer such as `` {{ hostvars | ip_from_inventory(hostname) }}``
     """
-    try:
-        return hostvars[hostname]['ansible_host']
-    except KeyError:
-        raise AnsibleError('hostname "{hostname}" not found in inventory'.format(hostname=hostname))
+    if not hostname in hostvars:
+        raise AnsibleError('<ip_from_inventory> hostname "{hostname}" not found in inventory'.format(hostname=hostname))
+
+    return hostvars[hostname]['ansible_host']
 
 class FilterModule(object):
     def filters(self):
